46a15b3ff97759f246d252dc63fb499d3490eeae
Emmanuel Gomez <gomezgtzemmanuel@gmail.com>
2021-09-22 03:49:43 -0500

    Se modifica front, slider, cards, loader, download-file, form, navbar y conexion con API. API tiene detalles, se comentan retweet y fav hasta conectar correctamente la busqueda del hashtag de search de front

--- API/index.js
+++ API/index.js
@@ -1,15 +1,57 @@
 const Twit = require("twit");
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const port = 3000;
+
+let hashtagArray = [];
+
+// #region
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(
+  express.json({
+    type: "*/*",
+  })
+);
+
+app.use(cors());
+// #endregion
+
+// Cuando te hagan un get
+app.get("/hashtag", (req, res) => {
+  res.send("Me hicieron un get");
+});
+
+// Cuando te hagan un post
+app.post('/hashtag', (req, res) => {
+  let transaction = req.body;
+  hashtagArray.push(transaction);
+  res.send(JSON.stringify("Hashtag guardado"));
+  for(let i=0; i<hashtagArray.length; i++) {
+  console.log(hashtagArray[i]);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Estoy ejecutandome en http://localhost:${port}`);
+});
 
 const T = new Twit({
-  consumer_key: "MJHQa8lNERAERQeuUj6KJMljh",//API KEY
-  consumer_secret: "j7uevbcxB9k71sTjzzYMYZYaYcfzhOnzFcqIrBN0qE4z1IFIE6",//API KEY SECRET
-  access_token: "95053840-kjXYR2Kwn930kND0NxHPDm6cnbeIPXitPtGlZqaX9",//ACCESS TOKEN
-  access_token_secret: "lxWc05ZGV3MEXw8dsZtgLJLQVh0GwOGEreY3UTowSmi67",//ACCESS TOKEN SECRET
+  consumer_key: "MJHQa8lNERAERQeuUj6KJMljh", //API KEY
+  consumer_secret: "j7uevbcxB9k71sTjzzYMYZYaYcfzhOnzFcqIrBN0qE4z1IFIE6", //API KEY SECRET
+  access_token: "95053840-kjXYR2Kwn930kND0NxHPDm6cnbeIPXitPtGlZqaX9", //ACCESS TOKEN
+  access_token_secret: "lxWc05ZGV3MEXw8dsZtgLJLQVh0GwOGEreY3UTowSmi67", //ACCESS TOKEN SECRET
   timeout_ms: 60 * 1000, // optional HTTP request timeout to apply to all requests.
   strictSSL: true, // optional - requires SSL certificates to be valid.
 });
-//Cuando alguien twittee el hashtag #gomezgtz2021 avisame
-const stream = T.stream("statuses/filter", { track: "#gomezgtz2021" });
+//Buscar los hashtag que vienen del arreglo hashtag array
+const stream = T.stream("statuses/filter", { track: `#${hashtagArray}` });
 
 // Obtener informacion del tweet
 stream.on("tweet", function (tweet) {
@@ -19,27 +61,27 @@ stream.on("tweet", function (tweet) {
   console.log(tweet.user.location);
   console.log(tweet.user.description);
 
-  // Retweet
-  T.post(
-    "statuses/retweet/:id",
-    { id: tweet.id_str },
-    function (err, data, response) {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log(`Se hizo retweet al tweet: ${tweet.id_str}`);
-      }
-    }
-  );
-  T.post(
-    "favorites/create",
-    { id: tweet.id_str },
-    function (err, data, response) {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log(`Se hizo favorito al tweet: ${tweet.id_str}`);
-      }
-    }
-  );
+  // // Retweet
+  // T.post(
+  //   "statuses/retweet/:id",
+  //   { id: tweet.id_str },
+  //   function (err, data, response) {
+  //     if (err) {
+  //       console.log(err);
+  //     } else {
+  //       console.log(`Se hizo retweet al tweet: ${tweet.id_str}`);
+  //     }
+  //   }
+  // );
+  // T.post(
+  //   "favorites/create",
+  //   { id: tweet.id_str },
+  //   function (err, data, response) {
+  //     if (err) {
+  //       console.log(err);
+  //     } else {
+  //       console.log(`Se hizo favorito al tweet: ${tweet.id_str}`);
+  //     }
+  //   }
+  // );
 });
---
+++ WEB/js/script.js
@@ -0,0 +1,15 @@
+const formElement = document.getElementById("searchHashtag");
+
+formElement.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let hashtag = document.getElementById("hashtag").value;
+  let transaction = {hashtag: hashtag};
+  let transactionJson = JSON.stringify(transaction);
+  // console.log(transactionJson);
+  // Mandar la transactionJson al backend y guardarlos ahi
+  fetch('http://localhost:3000/hashtag', {
+    method: 'POST',
+    body: transactionJson
+  });
+  // .then(x=> console.log('Hola'))
+})
